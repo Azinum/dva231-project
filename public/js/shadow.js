040804bd5ae24e245d31bd0330cc464b2a7f6bad
Linus W. Monie <zerox7felf@gmail.com>
2020-10-18 19:45:09 +0200

    better approack to tab shadows, shadows load correctly in tabs

--- public/js/shadow.js
+++ public/js/shadow.js
@@ -4,10 +4,12 @@ function update_shadows() {
         let shadow = shadowbox.querySelector(".shadow");
         let size = target.getBoundingClientRect();
 
+        console.log(size);
+
         shadow.style.width = size.width + "px";
         shadow.style.marginLeft = window.getComputedStyle(target).getPropertyValue("margin-left");
     });
 }
 
-window.addEventListener("load", update_shadows);
+document.addEventListener("load", update_shadows);
 window.addEventListener("resize", update_shadows);
