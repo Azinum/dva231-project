ee391c096169cd83f80a8c37def47887e56cbf6d
Linus W. Monie <zerox7felf@gmail.com>
2020-10-18 02:52:24 +0200

    shadow example

---
+++ public/shadowtest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/shadow.css">
+        <script src="/js/shadow.js"></script>
+    </head>
+    <body>
+        <div class="shadowbox" style="width: 50%;">
+            <p class="shadowparent" style="border-radius:0.5vw; color:white; background:gray; padding: 20px; height: 5vw; text-align:center;">
+                Hejsan! Jag har en skugga
+            </p>
+            <div class="shadow"></div>
+        </div>
+    </body>
+</html>
