6499dd8da4ad413823aef8dd313db71cdaee2712
Martin Orell <martin.orell92@gmail.com>
2020-10-20 19:32:40 +0200

    navbar fixed

---
+++ public/navbar_final.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<head>
+	<link href= "css/common.css" rel="stylesheet">
+	<link href= "css/navbar.css" rel="stylesheet">
+</head>
+<body>
+
+	<div class= "navbar">
+		<a href= "home.php" > Start match <img src="img/startmatch.png" class="nav-image" > </a>
+		<a href= "home.php" > My Teams <img src="img/teamicon.png" class="nav-image" > </a>
+		<a href="home.php"> Search <img src="img/searchicon.webp" class="nav-image" > </a>
+
+		<div class="dropdown">
+			<a href= "login.php"> My Profile <img src="img/profileicon.png" class="nav-image" > </a>
+				<div class="dropdown-content">
+					<a href="logout.php">Log out</a>
+				</div>
+		</div> 
+	</div>
+	
+</body>
