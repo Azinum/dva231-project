7474f5c5955d9915b6586f6e5a3ffbd9eed9810b
azinum <lukaska61@gmail.com>
2020-10-21 21:33:06 +0200

    Use global callback function instead of custom event

--- public/js/match.js
+++ public/js/match.js
@@ -2,65 +2,6 @@
 
 var searchOverlay = false;
 
-/*
-const TargetType = {
-	SELECT_TEAM : 0,
-	SELECT_PLAYER : 1,
-};
-
-const Teams = {
-	TEAM1 : 0,	// NOTE(lucas): Team 1 is always your team when modifying match results.
-	TEAM2 : 1,
-};
-
-var Team = function(teamName) {
-	this.teamName = teamName;
-	this.teamMembers = [];
-}
-
-var TeamMember = function(name) {
-	this.name = name;
-}
-
-var teams = [
-	new Team("Team 1"),
-	new Team("Team 2")
-];
-
-var target = {
-	elem : null,
-	team : null,
-	targetType : -1,
-}
-
-function selectTarget(elem, team, targetType) {
-	target.elem = elem;
-	target.team = team;
-	target.targetType = targetType;	// NOTE(lucas): Are we selecting a team or team player?
-}
-
-function setTarget(elem) {
-	if (!target) {
-		return;
-	}
-	let img = elem.querySelector("img");
-	let name = elem.querySelector("p");
-	target.elem.src = img.src;
-
-	if (target.targetType == TargetType.SELECT_TEAM) {
-		target.team.teamName = name.innerText;
-	}
-	else if (target.targetType == TargetType.SELECT_PLAYER) {
-		// target.team.teamName = name.innerText;
-	}
-}
-
-function searchResultsUpdate() {
-
-}
-
-*/
-
 const TargetType = {
 	TARGET_PLAYER : 0,
 	TARGET_TEAM : 1
@@ -84,45 +25,42 @@ function toggleOverlay() {
 	}
 }
 
+var onClickEvent = () => {};
+
 function searchOverlayUpdate() {
 
 }
 
 function selectPlayer(elem) {
 	doSearch(TargetType.TARGET_PLAYER, (e) => {
-		elem.src = e.detail.img;
+		elem.src = e.data.img;
 	});
 }
 
 function selectTeam(elem) {
 	doSearch(TargetType.TARGET_TEAM, (e) => {
-		elem.src = e.detail.img;
+		elem.src = e.data.img;
 	});
 }
 
 function doSearch(target, callback) {
 	toggleOverlay();
 	if (target == TargetType.TARGET_PLAYER) {
-		document.addEventListener("searchDone", callback);
+		onClickEvent = callback;
 	}
 	else if (target == TargetType.TARGET_TEAM) {
-		document.addEventListener("searchDone", callback);
+		onClickEvent = callback;
 	}
 }
 
 function onClick(img, name) {
 	toggleOverlay();
-	document.dispatchEvent(
-		new CustomEvent(
-			"searchDone",
-			{
-				detail: {
-					img: img,
-					name: name
-				}
-			}
-		)
-	);
+	onClickEvent({
+		data: {
+			img: img,
+			name: name
+		}
+	});
 }
 
 ((func) => {
