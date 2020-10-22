aead31eb905b3ffb33d396d4aea1f6e5f6a6d253
GrillexN <alfons.jansson98@gmail.com>
2020-10-22 18:23:42 +0200

    Sign up working, post signup added

---
+++ public/post_signup.php
@@ -0,0 +1,23 @@
+<?php session_start();
+require_once("../dbfunctions/register_user.php");
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Scoreboard: Sign up</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/signup.css">
+  <link rel="stylesheet" href="css/common.css">
+</head>
+
+<body>
+    <?php include("navbar_final.php"); ?>
+    <div class="ui-box shadow" id="back-box">
+        <h2>Sign up</h2>
+        <?php register_user() ?>
+    </div>
+</body>
+
+</html>
--- public/signup.php
+++ public/signup.php
@@ -1,3 +1,6 @@
+<?php session_start();
+require_once("../dbfunctions/register_user.php");
+?>
 <!DOCTYPE html>
 <html>
 
@@ -14,18 +17,18 @@
     <div class="ui-box shadow" id="back-box">
         <h2>Sign up</h2>
         <table>
-            <form method="post">
+            <form method="post" action="post_signup.php"> <!-- Kolla på en TILL signup där den tar emot en get? -->
                 <tr>
                     <td><input class="text-input-field" type="text" name="username" placeholder="Username"></td>
                     <td><input class="text-input-field" type="password" name="password" placeholder="Password"></td>
                 </tr>
                 <tr>
                     <td><input class="text-input-field" type="text" name="email" placeholder="Email"></td>
-                    <td><input class="text-input-field" type="password" name="cnfrmpassword" placeholder="Confirm Password"></td>
+                    <td><input class="text-input-field" type="password" name="cnfrmpassword" placeholder="Confirm Password"></td> <!-- Confirm password med javascript -->
                 </tr>
                 <tr>
                     <td> </td>
-                    <td><input class="button button-submit" type="submit" value="Sign Up"></td>
+                    <td><input class="button button-submit" name="signup" type="submit" value="Sign Up"></td> <!-- Javascript? -->
                     <td> </td>
                 </tr>
             </form>
