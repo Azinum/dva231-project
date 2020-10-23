0dc89515fdf9e2fb454f9a90967b4bba5221c3d3
GrillexN <alfons.jansson98@gmail.com>
2020-10-23 19:21:26 +0200

    teamsbyrank bygger sida

--- public/home.php
+++ public/home.php
@@ -4,6 +4,7 @@ require_once("../dbfunctions/get_specteaminfo.php");
 require_once("../dbfunctions/get_specuserinfo.php");
 require_once("../dbfunctions/get_userteams.php"); //Ta bort denna efter testing
 require_once("../dbfunctions/dbconnection.php"); //Ta bort denna efter testing
+require_once("../dbfunctions/get_usermatches.php"); //Ta bort denna efter testing
 require_once("../dbfunctions/get_teamsbyrank.php"); //Ta bort denna efter testing
 
 ?>
@@ -38,72 +39,7 @@ require_once("../dbfunctions/get_teamsbyrank.php"); //Ta bort denna efter testin
                     <div class="flextable">
                         <!-- Table begin -->
 
-                        <div class="flex-row">
-                            <!-- Table row 1 begin -->
-                            <!--Php funktion ska spotta ut sig hela tr för varje entry i teamdb, med information som matchar -->
-
-                            <?php
-                            profile_box_member([
-                                "name" => "Teamname",
-                                "img_url" => "/img/teamico.png",
-                                "stats" => ["won" => 13, "lost" => 3, "part" => 7],
-                                "rank" => 1,
-                                "score" => 9001
-                            ],
-                            [
-                                "show_stats" => true,
-                                "img_small" => true,
-                                "stats_short" => true,
-                                "show_rank" => true,
-                                "show_score" => true,
-                                "buttons" => [
-                                    "kick" => false
-                                ]
-                            ]);
-                            ?>
-                        </div> <!-- Table row 1 end -->
-                        <div class="flex-row">
-                            <!-- Table row 2 begin -->
-                            <!--Har max-värden i namn, overflow startar för ranknummer vid 10 000. -->
-                            <?php
-                            profile_box_member([
-                                "name" => "2nrdXuWOMHcFqMYzVDAFmZeQWeagZYugEuEA8cPN1G19q4TTXq",
-                                "img_url" => "/img/teamico.png",
-                                "stats" => ["won" => 9999, "lost" => 9999, "part" => 19998],
-                                "score" => 400,
-                                "rank" => 9999,
-                            ],
-                            [
-                                "show_stats" => true,
-                                "img_small" => true,
-                                "stats_short" => true,
-                                "show_rank" => true,
-                                "show_score" => true,
-                                "buttons" => [
-                                    "kick" => false
-                                ]
-                            ]);
-                            ?>
-                        </div> <!-- Table row 2 begin -->
-                        <div  class="flex-row">
-                            <!--Har max-värden i namn, overflow startar för ranknummer vid 10 000. -->
-                            <?php
-                            //$info = get_specteaminfo("TeamName");
-                            $info["img_small"] = true;
-                            $info["show_stats"] = true;
-                            $info["show_score"] = false; //Ändra när ELO i DB är fixat
-                            $info["stats_short"] = true;
-                            $info["show_rank"] = true;
-                            $info["img_small"] = [
-                                    "leave" => false,
-                                    "invite_controls" => false
-                            ];
-                            $info["buttons"] = [
-                                "kick"=> false
-                            ];
-                            profile_box_member (get_specteaminfo($link, "TeamName"),$info);
-                            ?>
-                        </div>
+                        <?php get_teamsbyrank($link) ?>
 
                     </div> <!-- Table end -->
                 </div>
@@ -112,7 +48,7 @@ require_once("../dbfunctions/get_teamsbyrank.php"); //Ta bort denna efter testin
     </table>
     <!-- <div class="bg"></div> -->
 
-    <?php get_teamsbyrank($link) ?>
+    <?php get_usermatches($link, 1) ?>
 
 </body>
 
