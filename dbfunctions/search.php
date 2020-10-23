2a15283ddec8657e2d070bd127f37540951d87de
azinum <lukaska61@gmail.com>
2020-10-23 17:02:48 +0200

    Add participant to team participant list when selecting a new team member

--- dbfunctions/search.php
+++ dbfunctions/search.php
@@ -20,7 +20,28 @@
         return $return;
     }
 
-	function search_users_in_team($link, $team) {
+	function search_users_in_team($link, $team, $query_string) {
+		$teamName = mysqli_real_escape_string($link, $team);
+		$userName = mysqli_real_escape_string($link, $query_string);
+		$query = 'SELECT * FROM User, TeamMemberships WHERE User.Id = TeamMemberships.Member AND TeamMemberships.TeamName = "' . $teamName .'" AND User.Username LIKE "%' . $userName . '%";';
+		$final_result = [];
+		if ($result = mysqli_query($link, $query)) {
+			while ($res_array = mysqli_fetch_assoc($result)) {
+				array_push($final_result, [
+					"name" => $res_array['Username'],
+					"mail" => $res_array['Email'],
+					"img_url" => $res_array['ProfileImageUrl'],
+					"bio" => $res_array['Bio'],
+					"is_admin" => $res_array['IsAdmin'],
+					"is_banned" => $res_array['IsBanned'],
+					"user_id" => $res_array['Id']
+				]);
+			}
+		}
+		return $final_result;
+	}
+
+	function get_users_in_team($link, $team) {
 		$teamName = mysqli_real_escape_string($link, $team);
 		$query = 'SELECT * FROM User, TeamMemberships WHERE User.Id = TeamMemberships.Member AND TeamMemberships.TeamName = "' . $teamName .'";';
 		$final_result = [];
