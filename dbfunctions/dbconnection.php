624bddf2a69d1a0bfc63230f58cb8b8014fa3c22
Linus W. Monie <zerox7felf@gmail.com>
2020-10-25 16:29:55 +0100

    dbconnection sends errors to error_log

--- dbfunctions/dbconnection.php
+++ dbfunctions/dbconnection.php
@@ -3,7 +3,7 @@
         $link= new mysqli("138.197.179.196:3306","scoreboard_account", "gimme them scorez yoo", "scoreboard_db");
 
         if (mysqli_connect_errno()) {
-            printf("Connect failed: %s\n", mysqli_connect_error());
+            error_log("Connect failed: %s\n", mysqli_connect_error());
             exit();
         }
 ?>
