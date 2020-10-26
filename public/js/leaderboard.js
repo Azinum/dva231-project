8cfc52bb16c4b4402d293f9923a142026a3f13fb
Linus W. Monie <zerox7felf@gmail.com>
2020-10-26 14:45:53 +0100

    leaderboard.js scroll stuff

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -16,32 +16,44 @@ function escapeHtml(text) {
 }
 
 function test() {
-
     fetch('/ajax/get_leaderboard.php/?s='+start).then((res) => res.json()) 
     .then(json => {
         //console.log(json);
-        json.forEach((item) => { //item ska bli skapelsen av ett item.... Gör en array av json värden, sedan skicka in dem i build leaderboard?
-            console.log(item);
-            document.getElementById("back-box").innerHTML += `
-            <div  class="flex-row">
-                <div class="profile-box ui-box shadow">
-                    <div class="profile">
-                        <div class="rank"> `+item.rank+`. </div>
-                        <div class="profilepic profilepic-small"> <img src="`+ escapeHtml(!item.img_url ? defaultProfile : item.img_url) +`"> </div>
-                    </div>
-                    <span class="label">`+escapeHtml(item.disp_name)+`</span>
-                    <div class="stats stats-short">
-                        <span> P:`+item.stats.part+` </span>
-                        <span> W:`+item.stats.won+` </span>
-                        <span> L:`+item.stats.lost+` </span>
-                    </div>
-                </div>
-            </div>
-                `;
-        });
-        
+		if (json) {
+			json.forEach((item) => { //item ska bli skapelsen av ett item.... Gör en array av json värden, sedan skicka in dem i build leaderboard?
+				document.getElementById("back-box").innerHTML += `
+					<div  class="flex-row">
+						<div class="profile-box ui-box shadow">
+							<div class="profile">
+								<div class="rank"> `+item.rank+`. </div>
+								<div class="profilepic profilepic-small"> <img src="`+ escapeHtml(!item.img_url ? defaultProfile : item.img_url) +`"> </div>
+							</div>
+							<span class="label">`+escapeHtml(item.disp_name)+`</span>
+							<div class="stats stats-short">
+								<span> P:`+item.stats.part+` </span>
+								<span> W:`+item.stats.won+` </span>
+								<span> L:`+item.stats.lost+` </span>
+							</div>
+						</div>
+					</div>
+				`;
+			});
+		}
     });
     start += length+1;
-
-    //.then('/ajax/build_leaderboard.php')
 }
+
+var isInLoadZone = false;
+window.addEventListener("scroll", (e) => {
+	let bottom = document.getElementById("back-box").getBoundingClientRect().bottom;
+	let height = document.documentElement.clientHeight;
+
+	if (bottom - 20 < height) {
+		if (!isInLoadZone) {
+			test();
+			isInLoadZone = true;
+		}
+	} else {
+		isInLoadZone = false;
+	}
+});
