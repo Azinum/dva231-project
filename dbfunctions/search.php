f43dfeec151c6ea0af691b3530964d89c49617bf
azinum <lukaska61@gmail.com>
2020-10-30 23:10:35 +0100

    Fix user sql search to exclude users who have a pending invite

--- dbfunctions/search.php
+++ dbfunctions/search.php
@@ -23,7 +23,7 @@
 	function search_users_in_team($link, $team, $query_string) {
 		$teamName = mysqli_real_escape_string($link, $team);
 		$userName = mysqli_real_escape_string($link, $query_string);
-		$query = 'SELECT * FROM User, TeamMemberships WHERE User.Id = TeamMemberships.Member AND User.IsDisabled IS NOT TRUE AND User.IsBanned IS NOT TRUE AND TeamMemberships.TeamName = "' . $teamName .'" AND User.Username LIKE "%' . $userName . '%";';
+		$query = 'SELECT * FROM User, TeamMemberships WHERE (TeamMemberships.Invitation IS NULL OR TeamMemberships.Invitation != 1) AND User.Id = TeamMemberships.Member AND User.IsDisabled IS NOT TRUE AND User.IsBanned IS NOT TRUE AND TeamMemberships.TeamName = "' . $teamName .'" AND User.Username LIKE "%' . $userName . '%";';
 		$final_result = [];
 		if ($result = mysqli_query($link, $query)) {
 			while ($res_array = mysqli_fetch_assoc($result)) {
