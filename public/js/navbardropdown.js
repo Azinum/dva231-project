6cb51be507057be8560f3232898ffd9529317c19
Martin Orell <martin.orell92@gmail.com>
2020-10-31 17:31:00 +0100

    dropdown script added

---
+++ public/js/navbardropdown.js
@@ -0,0 +1,13 @@
+
+
+function toggleDropDown(){
+	var dropdown = document.getElementById("dropdowncontentid");
+	if(dropdown.style.display == "block")
+	{
+		dropdown.style.display = "none";
+	}
+	else
+	{
+		dropdown.style.display = "block";
+	}
+}
