a987323cf095bd21c760bded964271db82fca0fd
Linus W. Monie <zerox7felf@gmail.com>
2020-10-31 20:01:26 +0100

    shit do fuck all on thing that don't matter to you

--- public/js/profile_public.js
+++ public/js/profile_public.js
@@ -1,12 +1,14 @@
 function smite_victim_team(victim) {
-    fetch("/ajax/set_ban_team.php?" + new URLSearchParams({
-        "team": victim
-    })).then((response) => {
-        if (response.status == 200) {
-            alert("Team smacked with heavy banhammer");
-            window.location.replace("/home.php");
-        } else {
-            alert("Evaded!");
-        }
-    });
+    if (confirm("Squash "+victim+" with banhammer?")) {
+        fetch("/ajax/set_ban_team.php?" + new URLSearchParams({
+            "team": victim
+        })).then((response) => {
+            if (response.status == 200) {
+                alert("Team smacked with heavy banhammer");
+                window.location.replace("/home.php");
+            } else {
+                alert("Evaded!");
+            }
+        });
+    }
 }
--- public/profile_public.php
+++ public/profile_public.php
@@ -115,7 +115,8 @@
                                 $match,
                                 [
                                     "verified" => true,
-                                    "lteam" => $lteam
+                                    "lteam" => $lteam,
+                                    "on_click" => "click_match(". intval($match["id"]) .")"
                                 ]
                             );
                         }
