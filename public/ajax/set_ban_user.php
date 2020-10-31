948d7a4219873beac0a3c483daaf5b937205f7b2
GrillexN <alfons.jansson98@gmail.com>
2020-10-31 19:38:56 +0100

    Thou shalt be smitten

---
+++ public/ajax/set_ban_user.php
@@ -0,0 +1,26 @@
+<?php
+    require_once("../../dbfunctions/dbconnection.php");
+    require_once("../../dbfunctions/get_specteaminfo.php");
+    require_once("../../dbfunctions/set_userban.php");
+
+    header("Content-type: application/json;");
+    if (isset($_GET["id"])) {
+
+        session_start();
+        //$teamdata = get_specteaminfo($link, $_GET["team"]);
+        if (!$_SESSION["isLoggedin"] || (!$_SESSION["admin"] )) {
+            http_response_code(403);
+            echo json_encode(["status" => "not authorized"]);
+            die();
+        }
+
+        $status = set_userban($link, $_GET["id"]);
+        if ($status) {
+            echo json_encode(["status" => "success"]);
+            die();
+        }
+    }
+    http_response_code(400);
+    echo json_encode(["status" => "error"]);    // Change for something more descriptive maybe...?
+
+?>
