457054c78a00a6d62fc8ce40af50dcf73d97ca46
Linus W. Monie <zerox7felf@gmail.com>
2020-11-01 18:38:47 +0100

    create, delete, verify match auth

--- public/ajax/create_match.php
+++ public/ajax/create_match.php
@@ -1,11 +1,20 @@
 <?php
 	require_once("../../dbfunctions/dbconnection.php");
+	require_once("../../dbfunctions/get_specteaminfo.php");
 	require_once("../../dbfunctions/modify_match.php");
 
 	header("Content-type: application/json;");
 
 	// TODO(lucas): Authorization checks!!!
-	if (isset($_GET["team1"]) && isset($_GET["team1"]) && isset($_GET["result"])) {
+	if (isset($_GET["team1"]) && isset($_GET["team2"]) && isset($_GET["result"])) {
+        $teamdata = get_specteaminfo($link, $_GET["team1"]);
+        session_start();
+        if (!$_SESSION["isLoggedin"] || ($_SESSION["uid"] != $teamdata["leader"] && !$_SESSION["admin"])) {
+			http_response_code(403);
+			echo json_encode(["status" => "not authorized"]);
+			exit();
+        }
+
 		$result = match_create($link, [
 			"team1" => $_GET["team1"],
 			"team2" => $_GET["team2"],
--- public/ajax/delete_match.php
+++ public/ajax/delete_match.php
@@ -1,20 +1,49 @@
 <?php
 	require_once("../../dbfunctions/dbconnection.php");
+	require_once("../../dbfunctions/get_specteaminfo.php");
+	require_once("../../dbfunctions/get_match.php");
 	require_once("../../dbfunctions/modify_match.php");
 
 	header("Content-type: application/json;");
 
 	// TODO(lucas): Authorization checks!!!
 	if (isset($_GET["id"])) {
-		if (match_delete($link, $_GET["id"])) {
-			echo json_encode(["status" => "success"]);
+        session_start();
+        if (!$_SESSION["isLoggedin"]) {
+			http_response_code(403);
+			echo json_encode(["status" => "not authorized"]);
 			exit();
-		}
-		else {
-			http_response_code(400);
-			echo json_encode(["status" => "error"]);
-			exit();
-		}
+        }
+        $match = get_match($link, $id);
+        if (!$match["is_verified"]) {
+            if (!$_SESSION["admin"]) {
+                if ($match["team2_should_verify"]) {
+                    $team2data = get_specteaminfo($link, $match["teams"][1]["name"]);
+                    if ($_SESSION["uid"] != $team2data["leader"]) {
+                        http_response_code(403);
+                        echo json_encode(["status" => "not authorized"]);
+                        exit();
+                    }
+                } else {
+                    $team1data = get_specteaminfo($link, $match["teams"][0]["name"]);
+                    if ($_SESSION["uid"] != $team1data["leader"]) {
+                        http_response_code(403);
+                        echo json_encode(["status" => "not authorized"]);
+                        exit();
+                    }
+                }
+
+                if (match_delete($link, $_GET["id"])) {
+                    echo json_encode(["status" => "success"]);
+                    exit();
+                }
+                else {
+                    http_response_code(400);
+                    echo json_encode(["status" => "error"]);
+                    exit();
+                }
+            }
+        }
 	}
 	http_response_code(400);
 	echo json_encode(["status" => "error"]);
